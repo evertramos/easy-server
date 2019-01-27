a6965d0ee37a32a92ca0f2ae2ea4d8d4dc90a9cb
Evert Ramos <evert.ramos@gmail.com>
2019-01-27 02:36:06 -0200

    Create docker-clean.sh

---
+++ bin/docker-clean.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Remove todos os containeres parados
+docker rm -v $(docker ps -a -q)
+
+# Remove todos Volumes não utilizados
+docker volume rm $(docker volume ls -qf dangling=true)
+docker volume ls -qf dangling=true | xargs -r docker volume rm
+
+# Remove todas as imagens não utilizadas
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+docker rmi -f $(docker images | grep "none" | awk '/ / { print $3 }')
+
+# Todas as images que não estão em uso
+docker rmi -f $(docker images | awk '/ / { print $3 }')
+
+
+#docker volume rm $(docker volume ls -qf dangling=true)
+
+#docker volume ls -qf dangling=true | xargs -r docker volume rm
+
+exit 0
