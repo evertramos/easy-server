0a0074157a0b5d5a04251421a731205154bfca5c
Evert Ramos <evert.ramos@gmail.com>
2020-01-31 08:54:26 -0300

    update script for better organization

--- install/nginx-proxy/install-proxy.sh
+++ install/nginx-proxy/install-proxy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Create a User Home root folder for webproxy
-mkdir ~/webproxy
-cd ~/webproxy
+mkdir ~/proxy
+cd ~/proxy
 
 # Clone repo
 git clone https://github.com/evertramos/docker-compose-letsencrypt-nginx-proxy-companion.git nginx-proxy
@@ -14,10 +14,11 @@ cd nginx-proxy
 cp .env.sample .env
 
 # Busca o IP da máquina
-NET_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+NET_IP=$(ip addr show ens3 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+#NET_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 
 # Substitute IP address in env file
-sed -i -e "s/0.0.0.0/$NET_IP/g" ~/webproxy/nginx-proxy/.env
+sed -i -e "s/0.0.0.0/$NET_IP/g" ~/proxy/nginx-proxy/.env
 
 # return to webproxy folder
 cd ..
@@ -30,11 +31,11 @@ cd nginx-data
 NGINX_DATA_PATH=$(pwd)
 
 # Substitue nginx files in .env
-sed -i -e "s%/path/to/your/nginx/data%$NGINX_DATA_PATH%g" ~/webproxy/nginx-proxy/.env
+sed -i -e "s%/path/to/your/nginx/data%$NGINX_DATA_PATH%g" ~/proxy/nginx-proxy/.env
 
 # Start nginx-proxy
 cd ../nginx-proxy
-./start.sh
+#./start.sh
 
 
 exit 0
