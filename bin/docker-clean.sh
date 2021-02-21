d184f376c6a9ad6bac4a753b1c8ab6e259f83910
Evert Ramos <evert.ramos@cgu.gov.br>
2021-02-21 12:23:54 -0300

    update docker clean

--- bin/docker-clean.sh
+++ bin/docker-clean.sh
@@ -3,22 +3,17 @@
 # Clean Everything in Docker Environment
 docker system prune --all
 
-# Remove todos os containeres parados
+# Remove all stopped containers
 docker rm -v $(docker ps -a -q)
 
-# Remove todos Volumes não utilizados
+# Remove all volumes not in use
 docker volume rm $(docker volume ls -qf dangling=true)
 docker volume ls -qf dangling=true | xargs -r docker volume rm
 
-# Remove todas as imagens não utilizadas
+# Remove all images not in use
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 docker rmi -f $(docker images | grep "none" | awk '/ / { print $3 }')
-
-# Todas as images que não estão em uso
-docker rmi -f $(docker images | awk '/ / { print $3 }')
-
-#docker volume rm $(docker volume ls -qf dangling=true)
-
-#docker volume ls -qf dangling=true | xargs -r docker volume rm
+#docker rmi -f $(docker images | awk '/ / { print $3 }')
 
 exit 0
+
