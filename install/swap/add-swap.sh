13826969bae245d87a3c871c40689dd4075af2d6
Evert Ramos <evert.ramos@cgu.gov.br>
2021-10-28 18:21:55 -0300

    Comment swap script

--- install/swap/add-swap.sh
+++ install/swap/add-swap.sh
@@ -2,35 +2,33 @@
 
 #SWAP_SIZE=1G
 SWAP_SIZE=4G
-#SWAP_SIZE=1048576
-#SWAP_SIZE=4194304
 
 # Test swap only create if there is no swap... or confirm with the user
 sudo swapon --show
 
-# Remove swapfile
-#sudo swapoff -v /swapfile
-# remove from /etc/fstab as well
-#sudo rm /swapfile
-
+# Allocate file with specified size
 sudo fallocate -l $SWAP_SIZE /swapfile
-#sudo dd if=/dev/zero of=/swapfile bs=1024 count=$SWAP_SIZE
 
+# Fix swap permissions
 sudo chmod 600 /swapfile
 
+# Create swap particion
 sudo mkswap /swapfile
 
+# Create the swap
 sudo swapon /swapfile
 
+# Save the swap at fstab to load when server restarts
 sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
 
+# Show the swap and memory
 sudo swapon --show
 sudo free -h
 
-
-cat /proc/sys/vm/swappiness
-#sudo sysctl vm.swappiness=10
-#cat /proc/sys/vm/swappiness
+# Update swappiness [?]
+# cat /proc/sys/vm/swappiness
+# sudo sysctl vm.swappiness=10
+# cat /proc/sys/vm/swappiness
 
 # In order to make persistent we need to update on /etc/sysctl.conf file
 # option vm.swappiness=10
