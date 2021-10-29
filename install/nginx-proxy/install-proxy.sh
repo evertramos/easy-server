3da5f41cf0bb4e10e3230b33ebccfcd59ba53e21
Evert Ramos <evert.ramos@cgu.gov.br>
2021-10-28 23:16:46 -0300

    Use nginx-proxy fresh install to create .env file

--- install/nginx-proxy/install-proxy.sh
+++ install/nginx-proxy/install-proxy.sh
@@ -13,9 +13,6 @@ cd $NGINX_PROXY_BASE_PATH
 git clone --recurse-submodules https://github.com/evertramos/nginx-proxy-automation.git $NGINX_PROXY_AUTOMATION_PATH
 cd $NGINX_PROXY_AUTOMATION_PATH
 
-# Create .env file
-cp .env.sample .env
-
 # Get current IP Address
 NET_INTERFACES=( eth0 ens3 )
 for i in "${NET_INTERFACES[@]}"; do
@@ -28,20 +25,14 @@ done
 # If we were able to identify the IP Address we will update the .env file
 if [[ ! $NET_IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   echo "We could not identify your IP Address, please update the .env and restart the nginx-proxy!"
-else
-  # Replace IP address in .env file
-  sed -i -e "s/0.0.0.0/$NET_IP/g" ${NGINX_PROXY_BASE_PATH%/}/${NGINX_PROXY_AUTOMATION_PATH%/}/.env
 fi
 
 # Create the nginx-proxy data path
 NGINX_NGINX_PROXY_DATA_PATH=${NGINX_PROXY_BASE_PATH%/}/${NGINX_PROXY_DATA_FILES%/}
 [[ ! -d "$NGINX_NGINX_PROXY_DATA_PATH" ]] && mkdir -p $NGINX_NGINX_PROXY_DATA_PATH && echo "The nginx-proxy data path '$NGINX_NGINX_PROXY_DATA_PATH' was created sucessfuly!"
 
-# Replace NGINX_DATA_PATH in .env file
-sed -i -e "s%./data%$NGINX_NGINX_PROXY_DATA_PATH%g" ${NGINX_PROXY_BASE_PATH%/}/${NGINX_PROXY_AUTOMATION_PATH%/}/.env
-
 # Start nginx-proxy with basic configuration
 cd ${NGINX_PROXY_BASE_PATH%/}/${NGINX_PROXY_AUTOMATION_PATH%/}/bin
-./fresh-start.sh --data-files-location=$NGINX_NGINX_PROXY_DATA_PATH --default-email=$NGINX_PROXY_EMAIL_ADDRESS --skip-docker-image-check --use-nginx-conf-files --update-nginx-template --yes --silent
+./fresh-start.sh --data-files-location=$NGINX_NGINX_PROXY_DATA_PATH --default-email=$NGINX_PROXY_EMAIL_ADDRESS --ip-address=$NET_IP --skip-docker-image-check --use-nginx-conf-files --update-nginx-template --yes --silent
 
 exit 0
