4accd0a9019aceca74b7de776b9b221767a9f82b
Evert Ramos <evert.ramos@gmail.com>
2022-02-05 13:25:15 -0300

    Update install-docker.sh

--- install/docker/install-docker.sh
+++ install/docker/install-docker.sh
@@ -21,6 +21,7 @@ sudo usermod -aG docker $USER
 #---------------------------------------------------------------------------------
 
 COMPOSE_VERSION=$(curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+AUTO_COMPLETE_COMPOSE_VERSION=1.29.2
 
 # Download the latest release of docker
 sudo curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
@@ -29,7 +30,7 @@ sudo curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VERSIO
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Install command completion
-sudo curl -L https://raw.githubusercontent.com/docker/compose/$COMPOSE_VERSION/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/$AUTO_COMPLETE_COMPOSE_VERSION/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 
 #---------------------------------------------------------------------------------
 # Check versions
