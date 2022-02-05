39ba5cfd0bd79724bb9382eeb147f180b6e14463
Evert Ramos <evert.ramos@gmail.com>
2022-02-05 19:06:39 -0300

    add server-automation instalation

---
+++ install/server-automation/install-server-automation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SERVER_AUTOMATION_BASE_FOLDER=~/server/server-automation
+
+# Create server-automation base path if it does not exists
+[[ ! -d "$SERVER_AUTOMATION_BASE_FOLDER" ]] && mkdir -p $SERVER_AUTOMATION_BASE_FOLDER && echo "The server-automation base path '$SERVER_AUTOMATION_BASE_FOLDER' was created sucessfuly!"
+
+# Clone server-automation latest version
+cd $SERVER_AUTOMATION_BASE_FOLDER
+git clone --recurse-submodules https://github.com/evertramos/server-automation.git .
+
+# @todo - automate the server-automation initial setup
+if [ -f "${SERVER_AUTOMATION_BASE_FOLDER}/README.md" ]; then
+  echo
+  echo "Success! Please create the .env file from the sample at ${SERVER_AUTOMATION_BASE_FOLDER}, and update at lease the BASE_SERVER_PATH variable."
+  echo 
+else
+  echo 
+  echo "It seems something went wrong... please try to clone server-automation manually, please access:" 
+  echo "https://github.com/evertramos/server-automation"
+  echo
+fi
+
+exit 0
+
